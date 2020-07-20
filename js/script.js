7ffafe3982b6201e9cfc0c9cdfae83fa24325b52
joao-pedro213 <joao.capobiango213@gmail.com>
2020-07-20 17:38:16 -0300

    Criando a estrutura HTML da aplicação.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+/* state variables */
+let searchInput = null;
+
+window.addEventListener('load', () => {
+  searchInput = document.querySelector('#search-input');
+  searchInput.focus();
+});
