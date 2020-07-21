08dfe59d740c748df8037b280c5657139f883507
joao-pedro213 <joao.capobiango213@gmail.com>
2020-07-20 23:54:53 -0300

    Criando a função loadData() e createUserList para recuperar os dados da API de usuários.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let ageSum = null;
 let ageAvg = null;
 
 let allUsers = [];
-let searchedUser = [];
+let allSearchedUsers = [];
 
 let totalMaleUsers = 0;
 let totalFemaleUsers = 0;
@@ -33,8 +33,40 @@ window.addEventListener('load', () => {
   femaleUsers.textContent = totalFemaleUsers;
   ageSum.textContent = totalAgeSum;
   ageAvg.textContent = totalAgeAvg;
+
+  loadData();
 });
 
-function doSearch() {
-  console.log('Searching...');
+async function loadData() {
+  try {
+    const res = await fetch(
+      'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
+    );
+    const json = await res.json();
+    createUserList(json);
+  } catch (err) {
+    console.log('Erro na requisição: ' + err);
+  }
 }
+
+function doSearch() {
+  console.log('searching...');
+
+  render();
+}
+
+function createUserList(data) {
+  allUsers = data.results.map((user) => {
+    const { name, lastName, picture, dob, gender } = user;
+
+    return {
+      name: name.first,
+      lastName: name.last,
+      picture: picture.thumbnail,
+      age: dob.age,
+      gender,
+    };
+  });
+}
+
+function render() {}
